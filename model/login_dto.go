0f77745f9114e8e26c759bc11e81fabca8820e67
stev97uce <estibenabc@gmail.com>
2025-06-20 19:05:00 -0500

    Limpieza total de la rama main

--- model/login_dto.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type LoginRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-}
--- model/register_dto.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type RegisterRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
-}
